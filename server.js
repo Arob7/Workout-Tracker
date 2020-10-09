591ee5675cc2c8c2837664ae7f47d96475184ed6
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-10-08 22:34:43 -0400

    added Express to server.js

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const mongoose = require("mongoose");
+const express = require("express");
+const logger = require("morgan");
 
-const Fitness = require("./workoutModel.js");
+const PORT = process.env.PORT || 3000;
+
+const Workout = require("./workoutModel.js");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
