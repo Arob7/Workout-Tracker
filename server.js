e537162e3b6e786da5db40bbc142086efbe359b0
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-10-08 22:14:25 -0400

    changed name from fitness to workout

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-const Fitness = require("./fitnessModel.js");
+const Fitness = require("./workoutModel.js");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbFitness", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
 });
