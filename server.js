24bf7f029b42221eb9d5e632d31dd6f4f078dad9
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-10-13 19:52:46 -0400

    changed Workout variable in seed.js and added empty routes files

--- server.js
+++ server.js
@@ -4,17 +4,24 @@ const logger = require("morgan");
 
 const PORT = process.env.PORT || 3000;
 
-const Workout = require("./workoutModel.js");
-
 const app = express();
 
 app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+// SET UP ROUTES
+
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
 });
