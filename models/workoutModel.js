f7040ce87f3480aea4d3a20661483e7fe26cd6ff
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-10-15 20:16:03 -0400

    added index.js to models, app.post error

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -1,2 +1,43 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: () => new Date(),
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Choose exercise: Resistance or Cardio",
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter name of exercise",
+      },
+      duration: {
+        type: Number,
+        required: "Enter exercise duration in minutes",
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    },
+  ],
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,9 +2,9 @@ const Workout = require("../models/workoutModel.js");
 
 module.exports = function (app) {
   app.get("/api/workouts", function (req, res) {
-    Workout.find()
-      .then((data) => {
-        res.json(data);
+    Workout.find({})
+      .then((db) => {
+        res.json(db);
       })
       .catch((err) => {
         res.json(err);
@@ -14,7 +14,7 @@ module.exports = function (app) {
   //   CREATES NEW WORKOUT
   app.post("/api/workouts", function (req, res) {
     Workout.create({})
-      .then((data) => res.json(data))
+      .then((db) => res.json(db))
       .catch((err) => {
         console.log("err", err);
         res.json(err);
@@ -24,8 +24,8 @@ module.exports = function (app) {
   //   UPDATES BY ID
   app.put("/api/workouts/:id", (req, res) => {
     Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } })
-      .then((data) => {
-        res.json(data);
+      .then((db) => {
+        res.json(db);
       })
       .catch((err) => {
         res.json(err);
@@ -35,8 +35,8 @@ module.exports = function (app) {
   //   DISPLAYS STATS
   app.get("/api/workouts/range", (req, res) => {
     Workout.find({})
-      .then((data) => {
-        res.json(data);
+      .then((db) => {
+        res.json(db);
       })
       .catch((err) => {
         res.status(400).json(err);
